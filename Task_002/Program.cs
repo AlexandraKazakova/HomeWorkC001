03cbd6b2cdd14b01580b469383f809c1c90043f2
Alexandra <sasha_huliganka@mail.ru>
2022-04-26 16:40:44 +0300

    Task002

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+Console.Write("Введите первое число -> ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число -> ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число -> ");
+int C = int.Parse(Console.ReadLine());
+if(A > B & A > C)
+{
+	Console.WriteLine("Максимальное из этих чисел ->" + A);
+}
+else if(B > A & B > C)
+{
+	Console.WriteLine("Максимальное из этих чисел ->" + B);
+}
+else
+{
+	Console.WriteLine("Максимальное из этих чисел ->" + C);
+}
