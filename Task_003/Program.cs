3155bfee608fcfee95ab83f9c2b860644dbaad19
Alexandra <sasha_huliganka@mail.ru>
2022-04-27 14:30:41 +0300

    Task003_1

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
 Console.Write("Введите число -> ");
 int num = int.Parse(Console.ReadLine());
-bool resalt = num % 2 == 0;
-Console.WriteLine(resalt ? "Число чётное" : "Число не чётное");
+bool result = num % 2 == 0;
+Console.WriteLine(result ? "Число чётное" : "Число не чётное");
