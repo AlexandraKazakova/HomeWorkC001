cf2989490d3e4af798ac82ad792e85ec052c455f
Alexandra <103699461+AlexandraKazakova@users.noreply.github.com>
2022-04-29 00:14:17 +0300

    Task_002_1

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
 Console.Write("Введите первое число -> ");
-int A = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число -> ");
-int B = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число -> ");
-int C = int.Parse(Console.ReadLine());
-if(A >= B & A >= C)
+int c = int.Parse(Console.ReadLine());
+if(a >= b && a >= c)
 {
-	Console.WriteLine("Максимальное из этих чисел ->" + A);
+	Console.WriteLine("Максимальное из этих чисел ->" + a);
 }
-else if(B >= A && B >= C)
+else if(b >= a && b >= c)
 {
-	Console.WriteLine("Максимальное из этих чисел ->" + B);
+	Console.WriteLine("Максимальное из этих чисел ->" + b);
 }
 else
 {
-	Console.WriteLine("Максимальное из этих чисел ->" + C);
+	Console.WriteLine("Максимальное из этих чисел ->" + c);
 }
